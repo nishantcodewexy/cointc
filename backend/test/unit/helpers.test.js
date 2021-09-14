b47a7f68739aeb6e4abd5d97af81ae5ed94b76e0
lil-armstrong <oebong1@gmail.com>
2021-09-15 00:48:30 +0100

    Added more helpers unit test

--- backend/test/unit/helpers.test.js
+++ backend/test/unit/helpers.test.js
@@ -2,47 +2,67 @@ const {
   generateReferralCode,
   sendMail,
   generateOTP,
-  textToHash,
-  hashIsText,
+  encrypt,
+  decrypt,
+  objectToHex,
+  hexToObject,
 } = require("../../src/helpers");
-const bcrypt = require("bcrypt");
+const database = require("../../src/database/models");
 const nodemailer = require("nodemailer");
-
-test(`generates referral code with the user's email at the start`, () => {
-  let email = "johndoe@mail";
-  expect(generateReferralCode(email)).toContain("johndoe");
-});
-
-test(`Compares text and its hash`, async () => {
-  let text = "12345678";
-
-  bcrypt.hash(text, 32, async (err, hash) => {
-    expect(typeof hash).toBe("string");
-
-    const compare = await hashIsText(text, hash);
-    expect(compare).toBeTruthy();
-  });
-});
-
-let otp_length = 6;
-let otp_code = generateOTP(otp_length);
-
-test(`Generates OTP with a max length of ${otp_length}`, () => {
-  expect(otp_code).toHaveLength(otp_length);
-});
-
-var testEmail = "test@mail";
+const test_password = "p@55w0rd";
+const test_email = "test@mail";
+const otp_length = 6;
+const otp_code = generateOTP(otp_length);
 const mailObject = {
-  to: testEmail,
+  to: test_email,
   from: "noreply@cryptcon",
   text: `Your OTP code is: ${otp_code}`,
   subject: "Cryptcon OTP",
 };
 
-test(`Generates OTP: ${otp_code}, and sends as mail to ${testEmail}`, async () => {
-  expect(sendMail).toBeInstanceOf(Function);
+it(`Connects to Database`, async () => {
+  let connected = false;
+  await database.sequelize.authenticate().then(() => (connected = true));
+  expect(connected).toBeTruthy();
+});
 
-  expect(sendMail(nodemailer.getTestMessageUrl(mailObject))).stringContaining(
-    "https://ethereal.email/message"
-  );
+describe(`Generates referral code`, () => {
+  const referral_code = generateReferralCode(test_email);
+  it(`Checks that referral code starts with the user's email`, () => {
+    expect(referral_code).toMatch(new RegExp(`^${test_email.split("@")[0]}`));
+  });
+});
+
+test(`Check text encryption and decryption`, async () => {
+  const encrypted = await encrypt(test_password);
+  const decrypted = await decrypt(test_password, encrypted)
+
+  console.log({encrypted });
+  expect(decrypted).toBe(true);
+
+  // it("Converts Object to HEX string", () => {
+  //   expect(typeof hex).toBe("string");
+  // });
+
+  // it("Converts HEX to Object string", () => {
+  //   expect(typeof obj).toBe("object");
+  // });
+
+});
+
+it(`Generates OTP with a max length of ${otp_length}`, () => {
+  expect(otp_code).toHaveLength(otp_length);
+});
+
+describe(`Checks the mailing feature`, () => {
+  it("Checks that sendMail is defined and is a function", () => {
+    expect(sendMail).toBeDefined();
+    expect(sendMail).toBeInstanceOf(Function);
+  });
+
+  // it(`Sends mail to ${test_email}`, async () => {
+  //   const mail_info = await sendMail(mailObject);
+
+  //   expect(typeof nodemailer.getTestMessageUrl(mail_info)).toBe('boolean')
+  // }, 1000);
 });
