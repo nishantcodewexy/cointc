242fbd2928bce14b2a961f7444530108f315e95e
lil-armstrong <oebong1@gmail.com>
2021-09-15 00:45:15 +0100

    Updated project config files

--- backend/src/database/config.js
+++ backend/src/database/config.js
@@ -1,27 +1,30 @@
-const fs = require('fs');
+const fs = require("fs");
 
 module.exports = {
   development: {
-    username: 'cryptcon',
-    password: 'cryptcon_pass',
-    database: 'cryptcon',
-    host: 'localhost',
-    port: 3306,
-    dialect: process.env.DB_DIALECT || 'postgre',
+    username: "cryptcon",
+    password: "cryptcon_password",
+    database: "cryptcon",
+    host: "localhost",
+    port: 5432,
+    dialect: process.env.DB_DIALECT || "postgres",
     dialectOptions: {
-      bigNumberStrings: true
-    }
+      bigNumberStrings: true,
+    },
+    logging: false
   },
   test: {
     username: process.env.CI_DB_USERNAME,
     password: process.env.CI_DB_PASSWORD,
     database: process.env.CI_DB_NAME,
-    host: '127.0.0.1',
-    port: 3306,
-    dialect: process.env.DB_DIALECT || 'postgre',
+    host: "127.0.0.1",
+    port: 5432,
+    dialect: process.env.DB_DIALECT || "postgres",
     dialectOptions: {
-      bigNumberStrings: true
-    }
+      bigNumberStrings: true,
+    },
+    logging: false
+
   },
   production: {
     username: process.env.PRODUCTION_DB_USERNAME,
@@ -29,12 +32,13 @@ module.exports = {
     database: process.env.PRODUCTION_DB_NAME,
     host: process.env.PRODUCTION_DB_HOSTNAME,
     port: process.env.PRODUCTION_DB_PORT,
-    dialect: process.env.DB_DIALECT || 'postgre',
+    dialect: process.env.DB_DIALECT || "postgres",
+    use_env_variable: 'DB_DIALECT',
     // dialectOptions: {
     //   bigNumberStrings: true,
     //   ssl: {
     //     ca: fs.readFileSync(__dirname + '/mysql-ca-master.crt')
     //   }
     // }
-  }
+  },
 };
