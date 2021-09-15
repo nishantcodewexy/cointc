81a8d6f44fb88741498cff42ac81e91da91f56f0
lil-armstrong <oebong1@gmail.com>
2021-09-15 17:42:59 +0100

    added referral code to seed

--- backend/src/database/seeders/20210914144446-demo-user.js
+++ backend/src/database/seeders/20210914144446-demo-user.js
@@ -9,6 +9,7 @@ module.exports = {
           id: '9fb01de0-1d63-4d09-9415-90e0b4e93b9a',
           email: "email@mail.com",
           password: "$2a$10$TuGJijoSAfLgHTneVCPQm.3XRYqoGEIQhLxwbIuFyvc13MGe6/YwK",
+          referral_code: 'seet7pcH'
         },
       ],
       {}
