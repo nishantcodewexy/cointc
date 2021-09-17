8307bb40505c0af2b71e9d29e41baed0f25ad5ea
lil-armstrong <oebong1@gmail.com>
2021-09-17 23:06:07 +0100

    Moved configs to helpers

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,104 +1,13 @@
 "use strict";
 
 const assert = require("assert");
-const nodemailer = require("nodemailer");
-const dotenv = require('dotenv');
-const fs = require('fs');
-const envConfig = dotenv.parse(fs.readFileSync('.env.local'))
-for (const k in envConfig) {
-  process.env[k] = envConfig[k]
-}
-// const env = process.env.NODE_ENVIRONMENT || 'development';
-
-// read in the .env file
+const { setupMailer, serverConfig, jwtConfig } = require("./helpers");
 
 try {
-  const {
-    PORT,
-    HOSTNAME,
-    HTTP_PROTOCOL,
-
-    EMAIL_USER,
-    EMAIL_PASSWORD,
-    EMAIL_PORT,
-    EMAIL_HOSTNAME,
-    EMAIL_SECURE,
-    SECRET_KEY,
-  } = process.env;
-
-  // validate the required configuration information
-  assert(SECRET_KEY, "SECRET_KEY env configuration is required.");
-
-  assert(PORT, "PORT env configuration is required.");
-  assert(HOSTNAME, "HOSTNAME env configuration is required.");
-
-  // assert( EMAIL_USERNAME, "EMAIL_USERNAME env configuration is required." );
-  // assert( EMAIL_PASSWORD, "EMAIL_PASSWORD env configuration is required." );
-  assert(EMAIL_HOSTNAME, "EMAIL_HOSTNAME env configuration is required.");
-  assert(EMAIL_PORT, "EMAIL_PORT env configuration is required.");
-
-  // Test mail account
-  let mailAccount =
-    EMAIL_USER && EMAIL_PASSWORD
-      ? {
-          user: EMAIL_USERNAME,
-          password: EMAIL_PASSWORD,
-        }
-      : nodemailer.createTestAccount();
-  
-  
-  // setup email transport config
-  const mailer = {
-    host: EMAIL_HOSTNAME,
-    port: EMAIL_PORT,
-    secure: EMAIL_SECURE, // true for 465, false for other ports
-    auth: {
-      user: mailAccount.user, // generated ethereal user
-      pass: mailAccount.pass, // generated ethereal password
-    },
-  };
-  
-  let mailTransporter = nodemailer.createTransport(mailer);
-  
-  // JWT Config
-  const jwt = {
-    keys: SECRET_KEY,
-    verify: false,
-    /* {
-      aud: "urn:audience:test",
-      iss: "urn:cryptcon:test",
-      sub: false,
-      nbf: true,
-      exp: true,
-      maxAgeSec: 14400, // 4 hours
-      timeSkewSec: 15,
-    }, */
-    validate: async (artifacts, request, h) => {
-     
-      const { server } = request;
-      const db = server.app.db;
-      const {
-        token,
-        decoded: {
-          payload: { user },
-        },
-      } = artifacts;
-
-      let foundToken = Boolean(await db.User.findOne({ where: {id: user} }));
-      return { isValid: foundToken, credentials: {} };
-    },
-  };
-
   module.exports = {
-    hostname: HOSTNAME,
-    port: PORT,
-    host_url: `${HTTP_PROTOCOL || "http"}://${HOSTNAME}:${PORT}`,
-    mailer: { ...mailer, transporter: mailTransporter },
-    jwt,
-    secret: SECRET_KEY
+    ...serverConfig,
+    mailer: setupMailer,
   };
-
-  module.exports.mailTransporter = mailTransporter;
-} catch (error) {
-  console.error(error);
+} catch (err) {
+  console.error(err);
 }
--- backend/src/helpers/index.js
+++ backend/src/helpers/index.js
@@ -1,13 +1,171 @@
 "use strict";
 
-const { mailTransporter } = require("../../src/config");
-const { nanoid } = require("nanoid");
-const bcrypt = require("bcryptjs");
 const assert = require("assert");
+const fs = require("fs");
+const path = require("path");
+const dotenv = require("dotenv");
+const { nanoid } = require("nanoid");
+const ejs = require("ejs");
+const bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
+const glob = require("glob");
 const Jwt = require("@hapi/jwt");
-const { secret } = require("../config");
+const envConfig = dotenv.parse(fs.readFileSync(".env.local"));
+
+for (const k in envConfig) {
+  process.env[k] = envConfig[k];
+}
+const {
+  PORT,
+  HOSTNAME,
+  HTTP_PROTOCOL,
+  SMTP_USER,
+  SMTP_PASS,
+  SMTP_PORT,
+  SMTP_HOST,
+  SMTP_SECURE,
+  SMTP_AUTH_TYPE,
+  SMTP_ACCESS_TOKEN,
+  SECRET_KEY,
+  CLIENT_URL,
+} = process.env;
+
+/****************************************************
+ * Validate the required configuration information
+ ****************************************************/
+assert(SMTP_USER, "SMTP_USER env configuration is required.");
+assert(SMTP_HOST, "SMTP_HOST env configuration is required.");
+assert(SECRET_KEY, "SECRET_KEY env configuration is required.");
+assert(PORT, "PORT env configuration is required.");
+assert(HOSTNAME, "HOSTNAME env configuration is required.");
+assert(CLIENT_URL, "CLIENT_URL env configuration is required.");
+
+const env = process.env.NODE_ENVIRONMENT || "development";
+
+/****************************************************
+ * JWT config
+ ****************************************************/
+function verifyJWTToken(token, secret = SECRET_KEY, options = {}) {
+  try {
+    Jwt.token.verify(token, secret, options);
+    return { isValid: true };
+  } catch (err) {
+    return {
+      isValid: false,
+      error: err.message,
+    };
+  }
+}
+
+const jwtConfig = {
+  keys: SECRET_KEY,
+  verify: false,
+  validate: async (artifacts, request, h) => {
+    const { server } = request;
+    const db = server.app.db;
+    const {
+      decoded: {
+        payload: { user },
+      },
+    } = artifacts;
+
+    let foundToken = Boolean(await db.User.findOne({ where: { id: user } }));
+    return { isValid: foundToken, credentials: {} };
+  },
+};
+
+/********************************************************
+ * Server config
+ * *****************************************************/
+const serverConfig = {
+  hostname: HOSTNAME,
+  port: PORT,
+  server_url: `${HTTP_PROTOCOL || "http"}://${HOSTNAME}:${PORT}`,
+  client_url: CLIENT_URL,
+  secret: SECRET_KEY,
+  jwt: jwtConfig,
+};
+
+/********************************************************
+ * Generates emailTemplates file content mapping object
+ * *****************************************************/
+const templateDir = "templates";
+const emailTemplates = {};
+const cwd = path.join(__dirname, "../..");
+
+glob
+  .sync(`*/${templateDir}/**/*.ejs`, {
+    root: cwd,
+  })
+  .forEach((file) => {
+    emailTemplates[String(path.parse(file).name.toLowerCase())] =
+      fs.readFileSync(path.join(cwd, file), {
+        encoding: "utf-8",
+      });
+  });
+
+/********************************************************
+ * Checks if email template exist
+ * *****************************************************/
+function hasEmailTemplate(name) {
+  assert(emailTemplates[name], `Email template: ${name} not found!`);
+  return emailTemplates[name];
+}
+
+/*************************************
+ * Maps email templates litrals to specified transform mapping
+ *************************************/
+const emailTemplateTransformer = (options) => {
+  let {
+    htmlTemplate,
+    subjectTemplate,
+    textTemplate,
+    html,
+    subject,
+    text,
+    ...rest
+  } = options;
+
+  const htmlContent = htmlTemplate && hasEmailTemplate(htmlTemplate.name);
+  const textContent = textTemplate && hasEmailTemplate(textTemplate.name);
+  const subjectContent =
+    subjectTemplate && hasEmailTemplate(subjectTemplate.name);
+
+  subject = subjectContent
+    ? ejs.render(subjectContent, subjectTemplate.transform)
+    : subject;
+
+  html = htmlContent ? ejs.render(htmlContent, htmlTemplate.transform) : html;
+
+  text = textContent ? ejs.render(textContent, textTemplate.transform) : text;
+
+  return {
+    ...rest,
+    html,
+    subject,
+    html,
+    text,
+  };
+};
 
 module.exports = {
+  serverConfig,
+  emailTemplateTransformer,
+  createWallet: async (password) => {
+    assert(password, "PASSWORD is reuired to create wallet");
+    // Connect to the Ethereum network
+    const web3 = new Web3(
+      Web3.givenProvider ||
+        new Web3.providers.HttpProvider("http://localhost:8545")
+    );
+
+    // Creates and returns new Ethereum account
+    return await web3.eth.personal.newAccount(password);
+  },
+
+  /****************************************************
+   * Generated referral code
+   ****************************************************/
   generateReferralCode(email) {
     let username = email.split("@")[0];
     let uuid = "";
@@ -18,6 +176,66 @@ module.exports = {
     return uuid + nanoid(4);
   },
 
+  /****************************************************
+   * Setup mailer
+   ****************************************************/
+  setupMailer: async () => {
+    try {
+      let account;
+
+      if (env == "development" && !(SMTP_PASS && SMTP_USER)) {
+        // Create Mail test account
+        const testMailAccount = await nodemailer.createTestAccount();
+        account = {
+          user: testMailAccount.user,
+          pass: testMailAccount.pass,
+        };
+      } else {
+        account = {
+          user: SMTP_USER,
+          pass: SMTP_PASS,
+
+          /***************
+           *For google email, remove password
+           ****************/
+          /* type: SMTP_AUTH_TYPE,
+          accessToken: SMTP_ACCESS_TOKEN */
+        };
+      }
+
+      // setup email transport config
+      const mailerOptions = {
+        host: SMTP_HOST || "smtp.ethereal.email",
+        port: SMTP_PORT || "587",
+        secure: SMTP_SECURE, // true for 465, false for other ports
+        auth: account,
+      };
+
+      let transporter = nodemailer.createTransport(mailerOptions, {
+        from: "Cryptcon <noreply@cryptcon.com>",
+      });
+
+      /**********************************************
+       * Generate email templates
+       ******************************************/
+
+      const transport = async (options, cb) =>
+        transporter.sendMail(emailTemplateTransformer(options), cb);
+
+      return {
+        sendMail: transport,
+        account,
+        templates: emailTemplates,
+      };
+    } catch (err) {
+      console.debug(`MailError: ${err}`);
+      debugger;
+    }
+  },
+
+  /****************************************************
+   * Text encryption
+   ****************************************************/
   encrypt: async (text) => {
     try {
       assert(text, "text is required");
@@ -28,7 +246,9 @@ module.exports = {
       console.error(err);
     }
   },
-
+  /***********************************************
+   * Checks if hash is text
+   ***********************************************/
   decrypt: async (text, hash) => {
     try {
       assert(text, "text is required");
@@ -39,6 +259,9 @@ module.exports = {
     }
   },
 
+  /***********************************************
+   * Generate text that can be used as OTP
+   ***********************************************/
   generateOTP: function (max = 4) {
     var digits = "0123456789";
     let OTP = "";
@@ -49,31 +272,18 @@ module.exports = {
     return OTP;
   },
 
-  sendMail: async function (mailOptions) {
-    const { from, to, subject, html, text } = mailOptions;
-    try {
-      assert(to, "sendMail to option is required.");
-      assert(from, "sendMail from option is required.");
-      assert(subject, "sendMail subject option is required.");
-      assert(text, "sendMail text option is required.");
+  // objectToHex(obj) {
+  //   return Buffer.from(JSON.stringify(obj)).toString("hex");
+  // },
 
-      // send mail with defined transport object
-      return await mailTransporter.sendMail(mailOptions);
-    } catch (err) {
-      console.error(err);
-      return false;
-    }
-  },
+  // hexToObject(hex) {
+  //   return JSON.parse(Buffer.from(hex, "hex"));
+  // },
 
-  objectToHex(obj) {
-    return Buffer.from(JSON.stringify(obj)).toString("hex");
-  },
-
-  hexToObject(hex) {
-    return JSON.parse(Buffer.from(hex, "hex"));
-  },
-
-  createToken(user) {
+  /***********************************************
+   * Creates JWT token
+   ***********************************************/
+  createJWTToken(user, expires = 14400) {
     let scopes = user.role;
 
     // Sign the JWT
@@ -84,25 +294,25 @@ module.exports = {
         iss: `urn:issuer:${scopes}`,
         group: scopes,
       },
-      secret,
+      SECRET_KEY,
       {
-        ttlSec: 14400,
+        ttlSec: expires,
       }
     );
   },
-
-  sluggify: (text) => {
-    return text.toLowerCase();
-  },
-
+  verifyJWTToken,
+  /***********************************************
+   * Gets the JWT token from a Hapi request object
+   ***********************************************/
   getJWTToken: (req) => {
     const {
-      auth: {
-        token
-      },
+      auth: { token },
     } = req;
     return token;
   },
+  /***********************************************
+   * Gets user from decoded JWT token
+   ***********************************************/
   getJWTDecodedUser: (req) => {
     const {
       auth: {
@@ -114,5 +324,9 @@ module.exports = {
       },
     } = req;
     return user;
+  },
+
+  sluggify: (text) => {
+    return text.toLowerCase();
   },
 };
