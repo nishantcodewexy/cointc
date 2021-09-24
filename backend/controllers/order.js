ded836f9374abf5eb8dfb412ad97dfa59ce7fa75
Micah Effiong <micaiah.effiong@gmail.com>
2021-09-24 14:33:24 +0100

    created order controller

---
+++ backend/controllers/order.js
@@ -0,0 +1,46 @@
+"use strict";
+const assert = require("assert");
+
+module.exports = (server) => {
+  const {
+    db: { Order },
+  } = server.app;
+
+  return {
+    async create(req) {
+      const { user } = req.pre.user;
+      return Order.create({ ...req.payload, from_user_id: user.id });
+    },
+
+    // Delete Order
+    async destroy(req) {
+      const { id } = req.payload;
+
+      return await Order.destroy({
+        where: id,
+        force: true,
+      });
+    },
+
+    // archive
+    async archive(req) {
+      const { id } = req.payload;
+
+      return await Order.destroy({
+        where: id,
+      });
+    },
+
+    // retrieve Order
+    async get(req) {
+      const { id } = req.payload;
+
+      return Order.findByPk(id);
+    },
+
+    // fetch all Orders
+    async getAll(req) {
+      return await Order.findAll();
+    },
+  };
+};
