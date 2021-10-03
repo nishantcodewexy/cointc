8e05b43ab117d89e3a03c8ab814453474cdffba4
Micah Effiong <micaiah.effiong@gmail.com>
2021-10-03 17:30:57 +0100

    added a single query route

---
+++ server/routes/wallet/getByAddress.js
@@ -0,0 +1,34 @@
+"use strict";
+
+module.exports = (server) => {
+  const {
+    controllers: {
+      wallet: { getByAddress },
+    },
+    helpers: {
+      jwt: { decodeUser },
+    },
+  } = server.app;
+
+  const schema = Joi.object({
+    address: Joi.string().required(),
+  });
+
+  return {
+    method: ["GET"],
+    path: "/wallet/{address}",
+    config: {
+      pre: [
+        {
+          method: decodeUser,
+          assign: "user",
+        },
+      ],
+      handler: getByAddress,
+      validate: {
+        params: schema,
+      },
+      auth: "jwt",
+    },
+  };
+};
