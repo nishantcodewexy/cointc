058a07372d4a317ba93c40cb04a439e0ce5e1551
lil-armstrong <oebong1@gmail.com>
2021-10-03 03:41:58 +0100

    Disabled other reducer listeners

--- client/src/jsx/_reducers/user.reducer.js
+++ client/src/jsx/_reducers/user.reducer.js
@@ -6,30 +6,30 @@ const initialState = null;
 
 export default function userReducer(state = initialState, action) {
   switch (action.type) {
-    case userConstants.PROFILE_REQUEST:
-      return {
-        status: 'requesting',
-      };
-    case userConstants.PROFILE_SUCCESS:
-      return {
-        status: 'completed',
-        data: action.user,
-      };
-    case userConstants.PROFILE_FAILURE:
-      return {
-        status: 'completed',
-        data: action.error
-      };
+    // case userConstants.PROFILE_REQUEST:
+    //   return {
+    //     status: 'requesting',
+    //   };
+    // case userConstants.PROFILE_SUCCESS:
+    //   return {
+    //     status: 'completed',
+    //     data: action.user,
+    //   };
+    // case userConstants.PROFILE_FAILURE:
+    //   return {
+    //     status: 'completed',
+    //     data: action.error
+    //   };
 
-    // Login
-    case userConstants.LOGIN_REQUEST:
-      return {
-        status: 'requesting',
-      };
+    // // Login
+    // case userConstants.LOGIN_REQUEST:
+    //   return {
+    //     status: 'requesting',
+    //   };
     
     case userConstants.LOGIN_SUCCESS:
       return {
-        status: 'completed',
+        // status: 'completed',
         data: action.user,
       };
 
