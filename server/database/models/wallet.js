7db3a8b2ddbe170b99938ba728ed241b0fbe26e2
Micah Effiong <micaiah.effiong@gmail.com>
2021-10-03 15:43:29 +0100

    configured wallet generator

--- server/database/models/wallet.js
+++ server/database/models/wallet.js
@@ -1,5 +1,6 @@
 "use strict";
 const { Model } = require("sequelize");
+const Tatum = require("@tatumio/tatum");
 
 module.exports = (sequelize, DataTypes) => {
   class Wallet extends Model {
@@ -12,22 +13,11 @@ module.exports = (sequelize, DataTypes) => {
       const { User, Wallet } = models;
       // User
       Wallet.belongsTo(User, {
-        foreignKey: { name: "owner_id" },
+        foreignKey: "owner_id",
       });
     }
-
-    // generate mnemonic and ex_pub
-    /**
-     * @name getMnemonicAndExPub
-     * @description generate mnemonic and extended public key
-     * @param {String} mnemonic
-     * @returns {Object} {mnemonic, xpub}
-     */
-    async getMnemonicAndExPub(mnemonic) {
-      return;
-    }
-    //
   }
+
   Wallet.init(
     {
       // owner_id: DataTypes.UUID,
@@ -42,8 +32,9 @@ module.exports = (sequelize, DataTypes) => {
       private_key: DataTypes.STRING,
       address: DataTypes.STRING,
       asset: {
-        type: DataTypes.STRING,
+        type: DataTypes.ENUM("BTC", "ETH", "BNB", "XRP", "USDT"),
         allowNull: false,
+        defaultValue: "BTC",
       },
     },
     {
@@ -51,7 +42,87 @@ module.exports = (sequelize, DataTypes) => {
       modelName: "Wallet",
       underscored: true,
       tableName: "tbl_wallets",
+      hooks: {
+        async beforeValidate(model, options) {
+          model.asset = model.asset.toUpperCase();
+          const {
+            mnemonic,
+            xpub,
+            key,
+            address,
+          } = await new WalletCreator().generate(model.asset);
+          model.extended_pub = xpub;
+          model.private_key = key;
+          model.address = address;
+          model.mnemonic = mnemonic;
+        },
+      },
     }
   );
   return Wallet;
 };
+
+class WalletCreator {
+  constructor(index = 0) {
+    this.newtworkType = process.env.NODE_ENV === "production";
+    this.index = index;
+  }
+
+  /**
+   * @name getMnemonicAndExPub
+   * @description generate mnemonic and extended public key
+   * @param {String} mnemonic string for generating wallet
+   * @param {String} currency crypto asset to be created
+   * @returns {Object} {mnemonic, xpub}
+   */
+  async #getMnemonicAndExPub(currency, mnemonic) {
+    const wallet = await Tatum.generateWallet(
+      Tatum.Currency[currency.toUpperCase()],
+      this.newtworkType, // network type
+      mnemonic
+    );
+    return wallet;
+  }
+
+  /**
+   * @name createPrivateKey
+   * @description generate wallet private key
+   * @param {String} currency crypto asset
+   * @param {String} mnemonic
+   * @returns {Object}
+   */
+  async #createPrivateKey(currency, mnemonic) {
+    const privateKey = await Tatum.generatePrivateKeyFromMnemonic(
+      Tatum.Currency[currency.toUpperCase()],
+      this.newtworkType, // network type
+      mnemonic,
+      this.index
+    );
+
+    return privateKey;
+  }
+
+  /**
+   * @name createWalletAddress
+   * @description create account wallet address
+   * @param {String} currency crypto asset
+   * @param {String} pubicKey wallet extended public key
+   * @returns {Object}
+   */
+  #createWalletAddress(currency, pubicKey) {
+    const address = Tatum.generateAddressFromXPub(
+      Tatum.Currency[currency.toUpperCase()],
+      this.newtworkType, // network type
+      pubicKey,
+      this.index
+    );
+    return address;
+  }
+
+  async generate(currency) {
+    const { mnemonic, xpub } = await this.#getMnemonicAndExPub(currency);
+    const key = await this.#createPrivateKey(currency, mnemonic);
+    const address = await this.#createWalletAddress(currency, xpub);
+    return { mnemonic, xpub, key, address };
+  }
+}
