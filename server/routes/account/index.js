f26ceb59bc75d4c1bfa34e13c4bcd01b9a3ac8c3
lil-armstrong <oebong1@gmail.com>
2021-10-03 03:50:37 +0100

    Code refactoring

--- server/routes/account/index.js
+++ server/routes/account/index.js
@@ -1,30 +1,34 @@
 "use strict";
 const Joi = require("joi");
-const kyc = require("./kyc");
 
 module.exports = (server) => {
   const {
     controllers: {
-      user: { profile, kyc, archive, update },
+      user: { profile, kyc, remove, update },
     },
     helpers: {
       jwt: { decodeUser },
     },
     boom,
+    db: {User},
     consts: { roles: _roles },
   } = server.app;
 
   return {
     method: ["GET", "DELETE", "PUT"],
-    path: "/account",
+    path: "/account/{kind?}",
     config: {
       pre: [
-        [
-          {
-            method: decodeUser,
-            assign: "user",
+        {
+          method: async (req) => {
+            return await User.findOne({
+              where: {
+                id: decodeUser(req),
+              },
+            });
           },
-        ],
+          assign: "user"
+        },
         {
           method: accountHandler,
           assign: "action",
@@ -39,74 +43,82 @@ module.exports = (server) => {
     // get the method of request
     let method = req.method.toLowerCase();
     // {POST, GET, PUT or DELETE}
-    switch (method) {
-      // GET requests
-      case "get": {
-        let { kind } = req.query;
+    const {
+      params: { kind },
+    } = req;
 
-        const schema = Joi.object({
-          role: Joi.string()
-            .error((err) => {
-              throw new Error('"role" required');
-            })
-            .required(),
-        });
-
-        try {
-          schema.validate(req.query);
-
-          switch (kind) {
-            case "kyc": {
+    try {
+      switch (kind) {
+        // KYC
+        case "kyc": {
+          switch (method) {
+            case "get":
+            default: {
               const schema = Joi.object({
-                type: Joi.string()
-                  .allow("email", "id", "phone", "payment_methods")
+                _type: Joi.string()
+                  .allow(
+                    "email",
+                    "id",
+                    "sms",
+                    "payment_methods",
+                    "OTP",
+                    "otp",
+                    "ID"
+                  )
                   .optional(),
               });
 
               let { error } = schema.validate(req.query);
-              if (error) return boom.badRequest("Bad request");
+              if (error) throw new Error("Bad Request Input");
+
               return kyc;
             }
+          }
+        }
+        // USER
+        case "user":
+        default: {
+          switch (method) {
+            case "put": {
+              const schema = Joi.object({
+                data: Joi.array().items(
+                  Joi.object({
+                    kyc: Joi.object({
+                      email: Joi.bool(),
+                      payment_methods: Joi.object({
+                        we_chat: Joi.any(),
+                      }),
+                      sms: Joi.string(),
+                      id: Joi.string(),
+                      account_no: Joi.string(),
+                      bank_name: Joi.string(),
+                      IFSC_code: Joi.string(),
+                      country: Joi.string(),
+                      currency: Joi.string(),
+                    }),
+                  })
+                ),
+              });
 
-            case "user":
+              let { error } = schema.validate(req.query);
+              if (error) throw new Error("Bad request input");
+
+              return update;
+            }
+
+            case "delete": {
+              return remove;
+            }
+
+            case "get":
             default: {
               return profile;
             }
           }
-        } catch (err) {
-          return boom.badRequest();
         }
       }
-
-      default: {
-        switch (method) {
-          case "delete": {
-            return archive;
-          }
-
-          case "put": {
-            const schema = Joi.object({
-              __update: Joi.object({
-                kyc: Joi.object({
-                  email: Joi.bool(),
-                  payment_methods: Joi.object({
-                    we_chat: Joi.any(),
-                  }),
-                  account_no: Joi.string(),
-                  bank_name: Joi.string(),
-                  IFSC_code: Joi.string(),
-                  country: Joi.string(),
-                  currency: Joi.string(),
-                  created_at: Joi.string(),
-                }),
-              }),
-            });
-            return update;
-          }
-        }
-      }
+    } catch (err) {
+      return boom.badRequest(err.message);
     }
-
-    return () => boom.boomify;
   }
 };
