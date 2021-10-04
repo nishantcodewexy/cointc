b04d1787e7493d222a7aab600136221bb41c89d6
Micah Effiong <micaiah.effiong@gmail.com>
2021-10-04 14:12:21 +0100

    fix

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 "use strict";
 
 const config = require("dotenv").config({
-  path: "./.env",
+  path: "../.env",
 });
 const env = process.env.NODE_ENV || "development";
 
-if (env === "development") console.log({ parsedEnv: config.parsed });
+// if (env === "development") console.log({ parsedEnv: config.parsed });
 if (config.error) {
   throw config.error;
 }
