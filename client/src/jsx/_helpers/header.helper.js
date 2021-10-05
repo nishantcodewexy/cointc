a071cacf68bb3f86028ec3da976b20935999691d
lil-armstrong <oebong1@gmail.com>
2021-10-05 14:36:13 +0100

    Added application/json content-type header

--- client/src/jsx/_helpers/header.helper.js
+++ client/src/jsx/_helpers/header.helper.js
@@ -1,9 +1,14 @@
 export default function header() {
   // return authorization header with jwt token
   var headers = new Headers();
-  let user = JSON.parse(localStorage.getItem("user")) || null;
+  let user = JSON.parse(localStorage.getItem("persist:root")) || null;
+  if (user?.session) {
+    console.log(user?.session);
+  }
+  headers.append("Content-Type", "application/json");
 
-  if (user && user?.token)
-    headers.append("Authorization", `Bearer ${user?.token}`);
+  if (user && user?.session)
+    headers.append("Authorization", `Bearer ${user.session?.token}`);
+    
   return headers;
 }
