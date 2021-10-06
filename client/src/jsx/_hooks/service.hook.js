5f581c0419bff63a831424d217de80feb97ae132
lil-armstrong <oebong1@gmail.com>
2021-10-06 16:00:43 +0100

    Added multi-service handlers - POST, PUT & DELETE

--- client/src/jsx/_hooks/service.hook.js
+++ client/src/jsx/_hooks/service.hook.js
@@ -1,38 +1,117 @@
 import { useState, useEffect } from "react";
 
 /**
- * @function useService - Component service hook
- * @param {Function} service
+ * @description - Specify the services to your component will be using. Usually (get, post, put and delete) request
+ * @typedef {Object} Services
+ * @property {Function} get  - get service
+ * @property {Function} post  - post service
+ * @property {Function} put  - put service
+ * @property {Function} drop  - drop/delete service
  * @returns
  */
-function useService(service) {
-  const [reload, dispatchReload] = useState(false);
+
+/**
+ * @function useService - Component request services hook
+ * @param {Services} services
+ * @param {Object} initialPayload - Initial service payload
+ * @param {Object | {getImmediate = true}} options - service hook options
+ * @returns
+ */
+function useService(
+  service,
+  initialPayload = {},
+  options = { getImmediate: true }
+) {
+  const [isReloading, setIsReloading] = useState(false);
   const [data, setData] = useState(false);
   const [error, setError] = useState(false);
   const [isFetching, setIsFetching] = useState(false);
+  const [_payload, setPayload] = useState(initialPayload);
 
-  const fetcher = async () => {
+  const request = async (service) => {
     // Only reload once
-    if (reload) {
-      dispatchReload(false);
-      return;
+    try {
+      if (isReloading) {
+        setIsReloading(false);
+        return;
+      }
+      setIsFetching(true);
+      let { success, failure } = await service();
+      success ? setData(success) : setError(failure);
+    } catch (error) {
+      console.error(error);
+      setError(error);
+    } finally {
+      setIsFetching(false);
     }
-    setIsFetching(true);
-    let { success, failure } = await service()
-    success ? setData(success) : setError(failure);
-    setIsFetching(false);
   };
+
+  /**
+   * @function dispatchRequest - request dispatcher
+   * @param {Object} request
+   * @param {String} request.type - Type of request [GET, POST, PUT, DELETE]
+   * @param {Object} request.payload - Request payload
+   * @returns
+   */
+  const dispatchRequest = ({ type, payload }) => {
+    setPayload(payload);
+
+    switch (String(type)?.toLowerCase()) {
+      case "post":
+      case "create":
+        request(() =>
+          service?.post ? service.post(_payload) : noService("post")
+        );
+        break;
+
+      case "put":
+      case "update":
+        request(() =>
+          service?.put ? service.put(_payload) : noService("put")
+        );
+        break;
+
+      case "drop":
+      case "delete":
+        request(() =>
+          service?.drop ? service.drop(_payload) : noService("drop, delete")
+        );
+        break;
+
+      case "get":
+      case "fetch":
+      default: {
+        request(() =>
+          service?.get ? service.get(_payload) : noService("get")
+        );
+        break;
+      }
+    }
+  };
+
+  const dispatchReload = () => setIsReloading(true);
 
   // Fetch on mount and watch for subsequent reloads requests
   useEffect(() => {
-    fetcher();
-  }, [reload]);
+    if (options.getImmediate) {
+      dispatchRequest({ type: "get" });
+    } else {
+      options.getImmediate = true;
+    }
+  }, [isReloading]);
 
   return {
     data,
     error,
     isFetching,
+    isReloading,
     dispatchReload,
+    dispatchRequest,
   };
+}
+
+function noService(type) {
+  let message = `No **${type}** service specified`;
+  throw new Error(message);
 }
 export default useService;
