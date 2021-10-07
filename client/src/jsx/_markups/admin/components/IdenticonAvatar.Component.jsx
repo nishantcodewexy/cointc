88f36180ef71d83c8a20bb62be00dcf477e25ea2
lil-armstrong <oebong1@gmail.com>
2021-10-07 20:17:20 +0100

    Set size in admin Identicon component

--- client/src/jsx/_markups/admin/components/IdenticonAvatar.Component.jsx
+++ client/src/jsx/_markups/admin/components/IdenticonAvatar.Component.jsx
@@ -1,13 +1,13 @@
 import { useState, useRef, useEffect } from "react";
 const identicon = require("identicon");
 
-function IdenticonAvatar({ id = "stranger", width, height }) {
+function IdenticonAvatar({ id = "stranger", size }) {
   const [buffer, setBuffer] = useState(null);
   const imgRef = useRef(null);
 
   useEffect(() => {
     try {
-      identicon.generate({ id, size: 150 }, (err, buffer) => {
+      identicon.generate({ id, size: size }, (err, buffer) => {
         if (err) throw err;
         imgRef.current.src = buffer;
       });
