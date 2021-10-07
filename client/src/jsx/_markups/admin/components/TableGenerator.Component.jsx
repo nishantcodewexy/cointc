3813428f8c08c6e88fb69f34c6c11de111b9bb3f
lil-armstrong <oebong1@gmail.com>
2021-10-07 15:05:22 +0100

    Added fullCols, fullRows states

--- client/src/jsx/_markups/admin/components/TableGenerator.Component.jsx
+++ client/src/jsx/_markups/admin/components/TableGenerator.Component.jsx
@@ -14,59 +14,97 @@ function TableGenerator({
   transformers = {},
   actions = {},
   mapping = {},
+  extras = [],
   omit = [],
 }) {
-  const [tableData, setTableData] = useState({ rows: [], cols: [] });
+  const [tableData, setTableData] = useState({
+    rows: [],
+    fullRows: [],
+    fullCols: [],
+    cols: [],
+  });
   const { selected, toggleSelect, checkedAll, bulkSelect } = useTableSelector();
 
   useEffect(() => {
-    const getMapping = () => [
-      data?.map((obj) => {
+    /**
+     * @function getMapping - Returns a row/col mapping of the table data
+     * @returns {[]}
+     */
+    function getMapping() {
+      let fullRows = data?.map((obj) => {
         let _obj = {};
-        let valid_entries = Object.entries(obj).filter(
-          ([key, value]) => !omit.includes(key)
-        );
-        console.log({ valid_entries });
-        valid_entries.forEach((entry) => (_obj[entry[0]] = entry[1]));
+        Object.entries(obj).forEach((entry) => (_obj[entry[0]] = entry[1]));
         return _obj;
-      }),
-      Object.keys(data[0])
-        .filter((itm) => !omit.includes(itm))
-        .map((key) =>
-          // console.log(key in mapping, { key, mapping }, mapping[key]),
-          key in mapping ? mapping[key] : key
-        ),
-    ];
+      });
+
+      let fullCols = Object.keys(data[0]).map((key) =>
+        key in mapping ? mapping[key] : key
+      );
+
+      return [
+        data?.map((obj) => {
+          let _obj = {};
+          let valid_entries = Object.entries(obj).filter(
+            ([key, value]) => !omit.includes(key)
+          );
+          // console.log({ valid_entries });
+          valid_entries.forEach((entry) => (_obj[entry[0]] = entry[1]));
+          return _obj;
+        }),
+        Object.keys(data[0])
+          .filter((itm) => !omit.includes(itm))
+          .map((key) =>
+            // console.log(key in mapping, { key, mapping }, mapping[key]),
+            key in mapping ? mapping[key] : key
+          ),
+        fullRows,
+        fullCols,
+      ];
+    }
 
     if (data?.length) {
-      const [rows, cols] = getMapping();
-      console.table(rows, cols);
-      console.log(rows, cols);
-      setTableData({ rows, cols });
+      const [rows, cols, fullRows, fullCols] = getMapping();
+
+      setTableData({ rows, cols, fullRows, fullCols });
     }
   }, [data]);
 
-  function transformValue(key, value) {
-    if (key in transformers) return transformers[key]({ key, value });
+  /**
+   * @function transformValue - Transforms column value using the transformer if specified
+   * @param {Object} modifiers
+   * @param {String} modifiers.key
+   * @param {String} modifiers.value
+   * @param {Object} modifiers.state
+   * @returns
+   */
+  function transformValue({ key, value, ...rest }) {
+    if (key in transformers) return transformers[key]({ key, value, ...rest });
     return String(value);
   }
 
-  const singleSelect = (id) => (
-    <div className={`custom-control custom-checkbox ml-2`}>
-      <input
-        type="checkbox"
-        className="custom-control-input "
-        id={`select_table_record_${id}`}
-        required=""
-        checked={selected.includes(id)}
-        onChange={() => toggleSelect(id)}
-      />
-      <label
-        className="custom-control-label"
-        htmlFor={`select_table_record_${id}`}
-      ></label>
-    </div>
-  );
+  /**
+   * @function singleSelect - Singularly selects a checkbox
+   * @param {String} id
+   * @returns
+   */
+  function singleSelect(id) {
+    return (
+      <div className={`custom-control custom-checkbox ml-2`}>
+        <input
+          type="checkbox"
+          className="custom-control-input "
+          id={`select_table_record_${id}`}
+          required=""
+          checked={selected.includes(id)}
+          onChange={() => toggleSelect(id)}
+        />
+        <label
+          className="custom-control-label"
+          htmlFor={`select_table_record_${id}`}
+        ></label>
+      </div>
+    );
+  }
 
   return tableData.rows.length ? (
     <Table responsive hover size="sm">
@@ -88,8 +126,12 @@ function TableGenerator({
               ></label>
             </div>
           </th>
-          {tableData.cols.map((col, key) => (
-            <th key={key}>{col.replace(/[_]/, " ")}</th>
+          {String(omit) !== "*" &&
+            tableData?.cols?.map((col, key) => (
+              <th key={key}>{String(col)?.replace(/[_]/, " ")}</th>
+            ))}
+          {extras?.map((extra, key) => (
+            <th key={key}>{String(extra)?.replace(/[_]/, " ")}</th>
           ))}
           <th>Action</th>
         </tr>
@@ -98,8 +140,16 @@ function TableGenerator({
         {tableData.rows.map((row, key) => (
           <tr key={key}>
             <td>{singleSelect(row?.id ?? key)}</td>
-            {Object.entries(row).map(([key, value], idx) => (
-              <td key={idx}>{transformValue(key, value)}</td>
+            {String(omit) !== "*" &&
+              Object.entries(row).map(([key, value], idx) => (
+                <td key={idx}>
+                  {transformValue({ key, value, row, state: tableData })}
+                </td>
+              ))}
+            {extras?.map((key, idx) => (
+              <td key={idx}>
+                {transformValue({ key, value: "", row, state: tableData })}
+              </td>
             ))}
             <td>{actions(row[0])}</td>
           </tr>
@@ -118,6 +168,8 @@ TableGenerator.propTypes = {
   actions: pt.func,
   mapping: pt.object,
   omit: pt.array,
+  extras: pt.array,
+  transformers: pt.object,
 };
 
 TableGenerator.defaultProps = {};
