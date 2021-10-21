636cf7fc21bfa40554edd8056dac55ef19451d21
lil-armstrong <oebong1@gmail.com>
2021-10-21 22:41:11 +0100

    Added client routes to server for production

--- server/plugins/app.plugin.js
+++ server/plugins/app.plugin.js
@@ -39,6 +39,96 @@ module.exports = {
         }, 
       },
     });
+    HapiServer.route({
+      method: "GET",
+      path: "/trade/{params*}",
+      handler: {
+        directory: {
+          path: '.',
+          redirectToSlash: true,
+        }, 
+      },
+    });
+    HapiServer.route({
+      method: "GET",
+      path: "/orders/{params*}",
+      handler: {
+        directory: {
+          path: '.',
+          redirectToSlash: true,
+        }, 
+      },
+    });
+    HapiServer.route({
+      method: "GET",
+      path: "/wallet/{params*}",
+      handler: {
+        directory: {
+          path: '.',
+          redirectToSlash: true,
+        }, 
+      },
+    });
+    HapiServer.route({
+      method: "GET",
+      path: "/affiliate/{params*}",
+      handler: {
+        directory: {
+          path: '.',
+          redirectToSlash: true,
+        }, 
+      },
+    });
+    HapiServer.route({
+      method: "GET",
+      path: "/support/{params*}",
+      handler: {
+        directory: {
+          path: '.',
+          redirectToSlash: true,
+        }, 
+      },
+    });
+    HapiServer.route({
+      method: "GET",
+      path: "/ad_create/{params*}",
+      handler: {
+        directory: {
+          path: '.',
+          redirectToSlash: true,
+        }, 
+      },
+    });
+    HapiServer.route({
+      method: "GET",
+      path: "/ad_payment_method/{params*}",
+      handler: {
+        directory: {
+          path: ".",
+          redirectToSlash: true,
+        },
+      },
+    });
+    HapiServer.route({
+      method: "GET",
+      path: "/ad_contract/{params*}",
+      handler: {
+        directory: {
+          path: ".",
+          redirectToSlash: true,
+        },
+      },
+    });
+    HapiServer.route({
+      method: "GET",
+      path: "/my-page/{params*}",
+      handler: {
+        directory: {
+          path: ".",
+          redirectToSlash: true,
+        },
+      },
+    });
   },
   name: "app",
   once: true,
