84c5413d3847b4887e90e14445cdb4e04436c0fe
popsicles <dan@vultr.guest>
2021-10-28 09:05:54 +0000

    removed views from prod server

--- server/views/js/orders.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $('#lnb .option li').click(function(e){
-        e.preventDefault();
-       $(this)
-                .addClass('on')
-            .siblings()
-                .removeClass('on');
-    });    
-});
--- server/views/js/wallet.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $('#asset .clear li').click(function(e){
-        e.preventDefault();
-       $(this)
-                .addClass('on')
-            .siblings()
-                .removeClass('on');
-    });    
-});
