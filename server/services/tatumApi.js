a6c4211fe3253c1a8466d11a5f00dae467f9712e
lil-armstrong <oebong1@gmail.com>
2021-11-11 09:08:22 +0100

    Fixed bug

--- server/services/tatumApi.js
+++ server/services/tatumApi.js
@@ -1,7 +1,6 @@
-const axios = require('axios').default;
-const assert = require("assert")
-const tatum = require("@tatumio/tatum")
-
+const axios = require("axios").default;
+const assert = require("assert");
+const tatum = require("@tatumio/tatum");
 
 /*
   mnemonic: 'organ merit dune hotel essence basic riot buzz dirt impact damage auto sing soap glow giraffe glare eye damage chunk orphan verify awake buyer',
@@ -10,307 +9,265 @@ const tatum = require("@tatumio/tatum")
   user_id = '086d48d9-24bf-4720-bf67-f41dd5d7553c'
  */
 
-  
-  
-    
-class OffChain{
-    /**
-     * 
-     * @param {axios} axios 
-     */
-    constructor(axios){
-        this.axios = axios
-        this.baseURL = "/offchain"
-    }
+class OffChain {
+  /**
+   *
+   * @param {axios} axios
+   */
+  constructor(axios) {
+    this.axios = axios;
+    this.baseURL = "/offchain";
+  }
 
+  /**
+   *
+   * @param {Object} args
+   * @param {Object} args.id account id
+   * @returns {Promise}
+   */
+  async createNewDepositAddress(args) {
+    const { id } = args;
 
-    /**
-     * 
-     * @param {Object} args
-     * @param {Object} args.id account id
-     * @returns {Promise}
-     */
-     async createNewDepositAddress(args){
-        const {id} = args
-        
-        return this.axios.request({
-            url:`${this.baseURL}/account/${id}/`,
-            method:"POST",
-        })
-    }
+    return this.axios.request({
+      url: `${this.baseURL}/account/${id}/`,
+      method: "POST",
+    });
+  }
 
-    /**
-     * 
-     * @param {Object} args
-     * @param {Object} args.id account id
-     * @returns {Promise}
-     */
-     async getAllDepositAddressForAccount(args){
-        const {id} = args
-        
-        return this.axios.request({
-            url:`${this.baseURL}/account/${id}/address`,
-            method:"GET",
-        })
-    }
+  /**
+   *
+   * @param {Object} args
+   * @param {Object} args.id account id
+   * @returns {Promise}
+   */
+  async getAllDepositAddressForAccount(args) {
+    const { id } = args;
 
-    /**
-     * 
-     * @param {Object} args
-     * @param {Object} args.id account id
-     * @param {Object} args.address address
-     * @returns {Promise}
-     */
-     async removeAddressFromAccount(args){
-        const {id,address} = args
-        
-        return this.axios.request({
-            url:`${this.baseURL}/account/${id}/address/${address}/`,
-            method:"DELETE",
-        })
-    }
+    return this.axios.request({
+      url: `${this.baseURL}/account/${id}/address`,
+      method: "GET",
+    });
+  }
 
+  /**
+   *
+   * @param {Object} args
+   * @param {Object} args.id account id
+   * @param {Object} args.address address
+   * @returns {Promise}
+   */
+  async removeAddressFromAccount(args) {
+    const { id, address } = args;
 
-    /**
-     * 
-     * @param {Object} args
-     * @param {Object} args.id account id
-     * @param {Object} args.address address
-     * @returns {Promise}
-     */
-     async assignAddressForAccount(args){
-        const {id,address} = args
-        
-        return this.axios.request({
-            url:`${this.baseURL}/account/${id}/address/${address}/`,
-            method:"POST",
-        })
-    }
+    return this.axios.request({
+      url: `${this.baseURL}/account/${id}/address/${address}/`,
+      method: "DELETE",
+    });
+  }
 
-    
+  /**
+   *
+   * @param {Object} args
+   * @param {Object} args.id account id
+   * @param {Object} args.address address
+   * @returns {Promise}
+   */
+  async assignAddressForAccount(args) {
+    const { id, address } = args;
 
-    /**
-     * 
-     * @param {Object} args
-     * @param {tatum.TransferBtcBasedOffchainKMS} args.data
-     * @returns {Promise<tatum.SignatureId>}
-     */
-     async sendBitcoinFromAccount(args){
-        const {data} = args
-        
-        return this.axios.request({
-            url:`${this.baseURL}/bitcoin/transfer/`,
-            method:"POST",
-            data
-        })
-    }
+    return this.axios.request({
+      url: `${this.baseURL}/account/${id}/address/${address}/`,
+      method: "POST",
+    });
+  }
 
-    
-    /**
-     * supported chain
-     * BTC,
-     * ETH,
-     * USDT,
-     * XRP
-     * EOS
-     * 
-     * @param {Object} param
-     * @param {tatum.Currency} param.currency
-     * @param {Object} param.data
-     */
-    async blockchainTransfer({currency,data}){
-        
-        /**
-         
-         */
-        let types = {
-            [tatum.Currency.BTC]:'bitcoin',
-            [tatum.Currency.ETH]:'ethereum',
-            // [tatum.Currency.USDT]:'ethereum',
-            [tatum.Currency.XRP]:'xrp',
-            // [tatum.Currency.EOS]:'xrp',
-        }
-        let type = types[currency]
-        if(!type) throw new Error(currency+" is not a support currency for tatum api")
+  /**
+   *
+   * @param {Object} args
+   * @param {tatum.TransferBtcBasedOffchainKMS} args.data
+   * @returns {Promise<tatum.SignatureId>}
+   */
+  async sendBitcoinFromAccount(args) {
+    const { data } = args;
 
-        const {data} = args
-        
-        return this.axios.request({
-            url:`${this.baseURL}/${type}/transfer/`,
-            method:"POST",
-            data
-        })
+    return this.axios.request({
+      url: `${this.baseURL}/bitcoin/transfer/`,
+      method: "POST",
+      data,
+    });
+  }
 
-    }
+  /**
+   * supported chain
+   * BTC,
+   * ETH,
+   * USDT,
+   * XRP
+   * EOS
+   *
+   * @param {Object} param
+   * @param {tatum.Currency} param.currency
+   * @param {Object} param.data
+   */
+  async blockchainTransfer({ currency, data }) {
+    let types = {
+      [tatum.Currency.BTC]: "bitcoin",
+      [tatum.Currency.ETH]: "ethereum",
+      // [tatum.Currency.USDT]:'ethereum',
+      [tatum.Currency.XRP]: "xrp",
+      // [tatum.Currency.EOS]:'xrp',
+    };
+    let type = types[currency];
+    if (!type)
+      throw new Error(currency + " is not a support currency for tatum api");
+    // const {data} = args
+    return this.axios.request({
+      url: `${this.baseURL}/${type}/transfer/`,
+      method: "POST",
+      data,
+    });
+  }
 }
 
+class Ledger {
+  /**
+   *
+   * @param {axios} axios
+   */
+  constructor(axios) {
+    this.baseURL = "/ledger";
 
-  
+    this.axios = axios;
+  }
 
-class Ledger{
-    /**
-     * 
-     * @param {axios} axios 
-     */
-    constructor(axios){
-        this.baseURL = "/ledger"
-       
-        this.axios = axios
-        
-        
-        
-    }
+  /**
+   *
+   * @param {Object} args
+   * @param {String} args.id
+   * @returns {Promise}
+   */
+  getAccountById(args) {
+    const { id } = args;
+    return this.axios.request({
+      url: `${this.baseURL}/account/${id}/`,
+    });
+  }
 
+  /**
+   *
+   * @param {Object} args
+   * @param {String} args.id
+   * @returns {Promise}
+   */
+  getAccountBalance(args) {
+    const { id } = args;
+    return this.axios.request({
+      url: `${this.baseURL}/account/${id}/balance`,
+    });
+  }
 
-    /**
-     * 
-     * @param {Object} args 
-     * @param {String} args.id
-     * @returns {Promise}
-     */
-    getAccountById(args){
-        const {id} = args
-        return this.axios.request({
-            url:`${this.baseURL}/account/${id}/`,
-            
-        })
-    }
+  /**
+   *
+   * @param {Object} args
+   * @param {String} args.id
+   * @returns {Promise}
+   */
+  freezeAccount(args) {
+    const { id } = args;
+    return this.axios.request({
+      url: `${this.baseURL}/account/${id}/freeze`,
+      method: "PUT",
+    });
+  }
 
-    
+  /**
+   *
+   * @param {Object} args
+   * @param {String} args.id
+   * @returns {Promise}
+   */
+  unfreezeAccount(args) {
+    const { id } = args;
+    return this.axios.request({
+      url: `${this.baseURL}/account/${id}/unfreeze`,
+      method: "PUT",
+    });
+  }
 
+  /**
+   *
+   * @param {Object} args
+   * @param {tatum.Account} args.data
+   * @returns {Promise}
+   */
+  createAccount(args) {
+    const { data } = args;
+    return this.axios.request({
+      url: `${this.baseURL}/account/`,
+      method: "POST",
+      data,
+    });
+  }
 
-    /**
-     * 
-     * @param {Object} args 
-     * @param {String} args.id
-     * @returns {Promise}
-     */
-    getAccountBalance(args){
-        const {id} = args
-        return this.axios.request({
-            url:`${this.baseURL}/account/${id}/balance`,
-        })
-    }
+  /**
+   *
+   * @param {Object} args
+   * @param {Object} args.params
+   * @param {Number} args.params.pageSize
+   * @param {Number} args.params.offset
+   * @param {String} args.params.accountCode
+   * @returns {Promise}
+   */
+  listAllAccounts(args) {
+    const { params = {} } = args;
 
-    /**
-     * 
-     * @param {Object} args 
-     * @param {String} args.id
-     * @returns {Promise}
-     */
-     freezeAccount(args){
-        const {id} = args
-        return this.axios.request({
-            url:`${this.baseURL}/account/${id}/freeze`,
-            method:"PUT"
-        })
-    }
+    return this.axios.request({
+      url: `${this.baseURL}/account/`,
+      method: "GET",
+      params,
+    });
+  }
 
+  /**
+   *
+   * @param {Object} args
+   * @param {Object} args.data
+   * @param {tatum.Account[]} args.data.accounts
+   * @returns {Promise}
+   */
+  createMultipleAccounts(args) {
+    const { data } = args;
 
-
-    /**
-     * 
-     * @param {Object} args 
-     * @param {String} args.id
-     * @returns {Promise}
-     */
-     unfreezeAccount(args){
-        const {id} = args
-        return this.axios.request({
-            url:`${this.baseURL}/account/${id}/unfreeze`,
-            method:"PUT"
-        })
-    }
-    
-
-    /**
-     * 
-     * @param {Object} args 
-     * @param {tatum.Account} args.data
-     * @returns {Promise}
-     */
-     createAccount(args){
-        const {data} = args
-        return this.axios.request({
-            url:`${this.baseURL}/account/`,
-            method:"POST",
-            data
-        })
-    }
-
-
-    /**
-     * 
-     * @param {Object} args
-     * @param {Object} args.params
-     * @param {Number} args.params.pageSize
-     * @param {Number} args.params.offset
-     * @param {String} args.params.accountCode
-     * @returns {Promise}
-     */
-     listAllAccounts(args){
-        const {params={}} = args
-        
-        return this.axios.request({
-            url:`${this.baseURL}/account/`,
-            method:"GET",
-            params
-        })
-    }
-
-
-    /**
-     * 
-     * @param {Object} args
-     * @param {Object} args.data
-     * @param {tatum.Account[]} args.data.accounts
-     * @returns {Promise}
-     */
-     createMultipleAccounts(args){
-        const {data} = args
-        
-        return this.axios.request({
-            url:`${this.baseURL}/account/`,
-            method:"GET",
-            data
-        })
-    }
-
-
-   
-
+    return this.axios.request({
+      url: `${this.baseURL}/account/`,
+      method: "GET",
+      data,
+    });
+  }
 }
-
-
 
 /**
- * this client is ment to intract with the api while the wallet service is use to 
+ * this client is ment to intract with the api while the wallet service is use to
  * to create methods to interface between the api client and coinTC
  */
 
-class TatumAPI{
-    constructor(){
-        // check if api key is available
-        assert(process.env.TATUM_API_KEY,"TATUM_API_KEY was not provided")
+class TatumAPI {
+  constructor() {
+    // check if api key is available
+    assert(process.env.TATUM_API_KEY, "TATUM_API_KEY was not provided");
 
-        this.axios = axios.create({
-            
-            baseURL: 'https://api-eu1.tatum.io/v3',
-            timeout: 1000,
-            headers: {
-                'x-api-key': process.env.TATUM_API_KEY,
-                'Content-Type': 'application/json'
-            }
-        });
+    this.axios = axios.create({
+      baseURL: "https://api-eu1.tatum.io/v3",
+      timeout: 1000,
+      headers: {
+        "x-api-key": process.env.TATUM_API_KEY,
+        "Content-Type": "application/json",
+      },
+    });
 
-        this.Ledger = new Ledger(this.axios)
-        this.OffChain = new OffChain(this.axios)
-
-
-    }
-
+    this.Ledger = new Ledger(this.axios);
+    this.OffChain = new OffChain(this.axios);
+  }
 }
 
-
-
-module.exports = new TatumAPI()
+module.exports = new TatumAPI();
